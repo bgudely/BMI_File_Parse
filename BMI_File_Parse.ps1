fc8aaac875a8bfb184c452db4cd51b43bfce7dd0
Brian Gudely <bgudely@paraport.com>
2016-06-17 12:32:37 -0700

    We have to substitute the country to A1

--- BMI_File_Parse.ps1
+++ BMI_File_Parse.ps1
@@ -1,13 +1,14 @@
-$workingDir   = 'H:\Scripts\'
-$newFile      = $workingDir + '20160105_SPACACP_CLS.SDC.xls'
-$emergingFile = $workingDir + 'Emerging-061416.xls'
-$outFile      = $workingDir + 'Emerging-Deleted.xls'
+$workingDir      = 'H:\Scripts\'
+$aShareInFile    = $workingDir + '20160105_SPACACP_CLS.SDC.xls'
+$aShareOutFile   = $workingDir + '20160105_SPACACP_CLS_new.SDC.xls'
+$emergingInFile  = $workingDir + 'Emerging-061416.xls'
+$emergingOutFile = $workingDir + 'Emerging-Deleted.xls'
 
 # Iterate through new file to pick up stock keys
 
-$file = New-Object System.IO.StreamReader($newFile)
+$file = New-Object System.IO.StreamReader($aShareInFile)
 
-$lineSkip = 1
+$lineSkip = 0
 $i = 0
 
 $stockKeys = @()
@@ -19,16 +20,34 @@ while (($line = $file.Readline()) -ne $null)
     if (($i -ge $lineSkip) -and ($column[0] -ne 'LINE COUNT:'))
     {
         $stockKeys += $column[12]
-    }
+
+        if ($i -ge 1)
+        {
+            $column[17] = 'A1'
+            $column[18] = 'A1'
+        }
+        
+        $outLine = $null;
+
+        for ($i = 1; $i -le 37; $i++)
+        {
+            $outLine += $column[$i] + "`t"
+        }
+
+        $outLine += $column[38]
+
+        $outLine | Out-File $aShareOutFile -append -encoding ASCII
+    }   
 
     $i++
 }
 
 $file.Close()
 
+
 # Remove lines from emerging file where stock key exists in new file
 
-$file = New-Object System.IO.StreamReader($emergingFile)
+$file = New-Object System.IO.StreamReader($emergingInFile)
 
 $lineSkip = 0
 $i = 0
@@ -44,7 +63,7 @@ while(($line = $file.ReadLine()) -ne $null)
         if ($stockKeys.Contains($stockKey) -ne $true)
         {
             $outLine = $line
-            $outLine | Out-File $outFile -append -encoding ASCII
+            $outLine | Out-File $emergingOutFile -append -encoding ASCII
         }
     }
 
